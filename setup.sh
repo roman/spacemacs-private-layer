516e74fb34ae8e14cc5c3fd46a395325b25f0c0b
Topher Brown <topher200@gmail.com>
2015-11-04 15:01:24 -0500

    whitespace nit

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ THIS_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 if ! [[ -e $HOME/.spacemacs ]]; then
 	echo "Link .spacemacs"
-	ln -s $THIS_DIR/spacemacs $HOME/.spacemacs	
+	ln -s $THIS_DIR/spacemacs $HOME/.spacemacs
 fi
 
 if ! [[ -L $HOME/.emacs.d/private ]] && [[ -d $HOME/.emacs.d/private ]]; then
